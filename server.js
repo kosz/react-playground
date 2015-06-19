ce1d97f0d5556c9852d0a4e1b1e34187a47600eb
Cosmin <coszmin@gmail.com>
2015-06-19 10:27:36 -0400

    fix(devEnv): jspm init from scratch, proper babel/jsx usage

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-var http = require('http')
-var express = require('express')
-var serveStatic = require('serve-static')'
-var finalhandler = require('finalhandler');;;;
+var http = require('http');
+var express = require('express');
+var serveStatic = require('serve-static');
 
 var port = process.env.PORT || 3000;
 
-var static = serveStatic('public', {'index': ['index.html']});
+var static = serveStatic('src', {'index': ['index.html']});
 
 var app = express();
 app.use(static);
 
-var server = http.createServer(app)
+var server = http.createServer(app);
 
 server.listen(port, function(error) {
-  if (error) return console.error(error)
+  if (error) { return console.error(error); }
   console.log('Listening at localhost:'+port);
-})
+});
